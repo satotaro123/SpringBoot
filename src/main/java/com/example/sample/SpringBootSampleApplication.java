94ca3cd194d5ef04c71fe0e808842abd56143a0e
sato taro <dsjvvknu@gmail.com>
2017-12-05 13:46:23 +0900

    l

--- src/main/java/com/example/sample/SpringBootSampleApplication.java
+++ src/main/java/com/example/sample/SpringBootSampleApplication.java
@@ -3,7 +3,7 @@ package com.example.sample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-//import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
 public class SpringBootSampleApplication {
